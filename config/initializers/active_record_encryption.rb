5343f63f9b47c22c44b4912b6a28c362875e76ac
Vladimir Pirvu <vladimir.pirvu@gmail.com>
2026-02-14 14:05:48 +0000

    Use dummy encryption keys in test environment for CI

--- config/initializers/active_record_encryption.rb
+++ config/initializers/active_record_encryption.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
-  config.active_record.encryption.primary_key = ENV.fetch("AR_ENCRYPTION_PRIMARY_KEY")
-  config.active_record.encryption.deterministic_key = ENV.fetch("AR_ENCRYPTION_DETERMINISTIC_KEY")
-  config.active_record.encryption.key_derivation_salt = ENV.fetch("AR_ENCRYPTION_KEY_DERIVATION_SALT")
+  if Rails.env.test?
+    # Use deterministic dummy keys for test environment
+    config.active_record.encryption.primary_key = "test" * 8
+    config.active_record.encryption.deterministic_key = "test" * 8
+    config.active_record.encryption.key_derivation_salt = "test" * 8
+  else
+    config.active_record.encryption.primary_key = ENV.fetch("AR_ENCRYPTION_PRIMARY_KEY")
+    config.active_record.encryption.deterministic_key = ENV.fetch("AR_ENCRYPTION_DETERMINISTIC_KEY")
+    config.active_record.encryption.key_derivation_salt = ENV.fetch("AR_ENCRYPTION_KEY_DERIVATION_SALT")
+  end
 
   # Allow reading unencrypted data during migration period
   config.active_record.encryption.support_unencrypted_data = true
