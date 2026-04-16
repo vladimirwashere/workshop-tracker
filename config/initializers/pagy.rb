90c6877f26bad40f68c21b4831975f48b6495d70
vlad <vladimir.pirvu@gmail.com>
2026-04-16 23:56:37 +0100

    fix: avoid mutating frozen pagy defaults on v43

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
-Pagy::DEFAULT[:limit] = 50
+pagy_config = defined?(Pagy::OPTIONS) ? Pagy::OPTIONS : Pagy::DEFAULT
 
-begin
-  require "pagy/extras/overflow"
-  Pagy::DEFAULT[:overflow] = :last_page
-rescue LoadError
-  # Pagy >= 43 removed the overflow extra and the :overflow variable.
-  Pagy::DEFAULT[:raise_range_error] = false
+# Pagy >= 43 freezes DEFAULT and uses OPTIONS for config.
+unless pagy_config.frozen?
+  pagy_config[:limit] = 50
+
+  begin
+    require "pagy/extras/overflow"
+    pagy_config[:overflow] = :last_page
+  rescue LoadError
+    # Pagy >= 43 removed the overflow extra and the :overflow variable.
+    pagy_config[:raise_range_error] = false
+  end
 end
